afcb1e942323c84866033c55cf92c7e6728597da
student <student@STEP.LAN>
2017-03-09 10:25:41 +0200

    Первый коммит - заготовка проэкта

---
+++ NodeManager/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NodeManager
+{
+    public partial class Form2 : Form
+    {
+        Form1 parent;
+
+        public Form2(Form1 x)
+        {
+            InitializeComponent();
+            this.parent = x;
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            parent.Close();
+        }
+    }
+}
